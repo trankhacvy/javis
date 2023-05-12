34eadf95a98a3f618a58e6f31a2af09321c17135
Khac Vy <khacvy93@gmail.com>
2023-05-12 14:44:40 +0700

    add policy

---
+++ src/website.tsx
@@ -0,0 +1,254 @@
+import "./assets/styles/tailwind.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div
+      className="jv-prose dark:jv-prose-dark"
+      style={{
+        maxWidth: "1024px",
+        margin: "40px auto",
+      }}
+    >
+      <div>
+        <h1 id="privacy-policy">Privacy Policy</h1>
+        <p>
+          This Privacy Policy ("Privacy Policy") outlines the manner in which
+          we, Javis and its affiliates ("Javis," "we," "our," or "us"), will
+          handle the personal information you ("User," "you") provide to us when
+          using our website,{" "}
+          <a href="https://javisgpt.vercel.app">https://javisgpt.vercel.app</a>{" "}
+          ("Website"), as well as our applications, browser extensions
+          ("Extension"), and services ("Services"). By using the Website or
+          Services, you are deemed to accept the terms of this Privacy Policy.
+          If you do not agree with these terms, please refrain from using the
+          services. In order to provide our Services, it is necessary for us to
+          collect and process certain personal information. We take reasonable
+          precautions to maintain the security of this information and prevent
+          unauthorized access or unlawful use. All personal information is
+          processed in accordance with data protection legislation.
+        </p>
+        <p>
+          Please read this Privacy Policy carefully to fully understand our
+          practices regarding Personal Data. "Personal Data" refers to any
+          information that can uniquely identify a living human being, either
+          alone or in combination with other data.
+        </p>
+        <p>
+          This Privacy Policy may be updated periodically, and any changes will
+          be effective from the date and time the revised Privacy Policy is
+          posted on the Website. We recommend checking back regularly for the
+          latest version of the Privacy Policy, as indicated below. If there are
+          any significant changes to the Privacy Policy, we will make reasonable
+          efforts to notify you through a notice on our Website or by other
+          means.
+        </p>
+        <h2 id="what-information-do-we-collect-and-how-is-it-used-">
+          What information do we collect and how is it used?
+        </h2>
+        <h3 id="when-you-create-an-account-and-use-our-services-as-a-registered-user">
+          When you create an account and use our services as a registered user
+        </h3>
+        <p>
+          When you create an account, log in, or use our services as a
+          registered user, we may request your email address or other details to
+          assist you. In addition to your email address, we may store your
+          hashed password, location, and IP address.
+        </p>
+        <h3 id="when-you-contact-us-e-g-customer-support-help-submit-a-request-blog-">
+          When you contact us (e.g., customer support, help, submit a request,
+          blog)
+        </h3>
+        <p>
+          If you contact us for customer support or post a question in our
+          discussion forum, we may collect the information you voluntarily
+          provide, including your full name, email address, location, device
+          type, operating system type, and any other information you choose to
+          provide.
+        </p>
+        <h2 id="how-do-we-use-your-information-">
+          How do we use your information?
+        </h2>
+        <p>
+          We use the collected information to perform and deliver the service,
+          as well as personalize your experience.
+        </p>
+        <h2 id="how-do-we-protect-and-store-your-personal-data-">
+          How do we protect and store your Personal Data?
+        </h2>
+        <p>
+          We regularly scan our website for security vulnerabilities and take
+          measures to ensure the safety of your personal information. Your
+          personal information is stored securely and is only accessible to a
+          limited number of authorized individuals who are obligated to maintain
+          confidentiality. Additionally, sensitive information you provide is
+          encrypted and transmitted using Secure Socket Layer (SSL) technology.
+          However, please note that we cannot guarantee absolute security
+          against unauthorized access to our servers. The security of your
+          information also depends on the computer, device, or network you use
+          to communicate with us and the security measures you employ to protect
+          your user IDs and passwords. Please take appropriate measures to
+          safeguard this information.
+        </p>
+        <p>
+          To maintain the security of your personal information, we implement
+          various security measures when you enter, submit, or access your
+          information or place an order. When you order our services or make a
+          payment, all transactions are processed through a secure gateway
+          provider, and your payment information is neither stored nor processed
+          on our servers.
+        </p>
+        <h2 id="how-we-handle-your-personal-information">
+          How We Handle Your Personal Information
+        </h2>
+        <p>
+          At our company, we prioritize the protection and responsible handling
+          of your personal information. In this section, we outline how we share
+          your data, our legal basis for processing data, the use of cookies,
+          processing user requests with OpenAI's API, subprocessors we engage
+          with, the use of external analytics tools, and how we handle data
+          retention, account deactivation, and deletion. Additionally, we
+          provide information on how you can contact us regarding any questions
+          or concerns about privacy.
+        </p>
+        <h3 id="sharing-your-personal-information">
+          Sharing Your Personal Information
+        </h3>
+        <p>
+          To ensure compliance with applicable laws, regulations, and rules, and
+          to respond to requests from law enforcement, regulatory agencies, or
+          court orders, we may share your personal information with regulators
+          on an as-needed basis. Furthermore, in the event that we sell or
+          transfer our business or assets to a third party, we may disclose
+          information to potential or actual purchasers to the extent required.
+          In situations involving the acquisition, merger, bankruptcy, or
+          similar events, we reserve the right to transfer or assign personal
+          data.
+        </p>
+        <p>
+          We may also share your personal data in the following circumstances:
+        </p>
+        <p>
+          With your consent: If you have provided consent for us to share your
+          personal data, such as marketing consents, we will do so accordingly.
+          Validated requests: Before disclosing any personal data in response to
+          information requests from law enforcement or regulators, we carefully
+          validate these requests. Legal Basis for Processing Data We collect,
+          use, and share data based on the following legal bases:
+        </p>
+        <p>
+          a) Fulfillment of Terms of Service: We process your data as necessary
+          to fulfill our Terms of Service. b) Compliance with Legal Obligations:
+          We process your data as necessary to comply with our legal
+          obligations. c) Consent: We process your data based on your consent,
+          which you can revoke at any time.
+        </p>
+        <h3 id="use-of-cookies">Use of Cookies</h3>
+        <p>
+          To provide you with a personalized experience and enable the smooth
+          functioning of our website and services, we may use "cookies,"
+          anonymous identifiers, and other tracking technologies. Cookies are
+          small text files that gather information about your activity on the
+          site. Most browsers allow you to control cookies, including accepting
+          or rejecting them and removing them from your browser settings.
+        </p>
+        <h3 id="processing-user-requests-with-openai-s-api">
+          Processing User Requests with OpenAI's API
+        </h3>
+        <p>
+          By utilizing our services and submitting requests, messages, or other
+          content, you agree to have your information processed through OpenAI's
+          API. OpenAI's API is developed and maintained by OpenAI Inc. When
+          using our services, any information you provide may be transferred and
+          processed by OpenAI to fulfill their service obligations. To learn
+          more about OpenAI's privacy practices, please refer to their Privacy
+          Policy at:{" "}
+          <a href="https://openai.com/policies/privacy-policy">
+            https://openai.com/policies/privacy-policy
+          </a>
+        </p>
+        <h4 id="subprocessors">Subprocessors</h4>
+        <p>
+          As part of providing our services, we engage third-party service
+          providers known as subprocessors to process personal information on
+          our behalf. Here is a list of our subprocessors:
+        </p>
+        <ul>
+          <li>
+            OpenAI: We use OpenAI to process your requests and utilize their
+            artificial intelligence services, including natural language
+            processing and generation. OpenAI processes and stores the content
+            you create and any data necessary for the functioning of AI models.
+          </li>
+          <li>
+            Paddle: For payment processing, we rely on Paddle, a third-party
+            payment processor. Paddle handles all aspects of the payment
+            process, including the collection and storage of payment
+            information.
+          </li>
+          <li>
+            Google Analytics: We utilize Google Analytics to collect and analyze
+            data related to the use of our website and services. This helps us
+            understand user behavior and improve our services.
+          </li>
+          <li>
+            PostHog: As an additional analytics tool, we employ PostHog to gain
+            insights into user interactions with our services. PostHog collects
+            and processes data about user behavior to optimize the user
+            experience. Use of External Analytics Tools To maintain
+          </li>
+        </ul>
+        <h2 id="use-of-external-analytics-tools">
+          Use of External Analytics Tools
+        </h2>
+        <p>
+          We use analytics tools (e.g. Google Analytics, PostHog) to collect
+          data about the use of the Website and Service. Analytics tools collect
+          data such as on how often users visit the Website or Services, what
+          pages they visit when they do so and what website they directly came
+          from to the Website or Service. We use the data we get from these
+          tools to maintain and improve the Website and Service and our other
+          products.
+        </p>
+        <h2 id="gdpr-data-protection-rights">GDPR Data Protection Rights</h2>
+        <p>
+          Under the General Data Protection Regulation, you have the right to
+          access, rectify, port and erase your data. You also have the right to
+          object to and restrict certain processing of your data. This includes
+          the right to object to our processing of your data for direct
+          marketing, which you can exercise by using the "unsubscribe" link in
+          such marketing communications.
+        </p>
+        <p>
+          If you’d like to exercise your rights or ask any questions about
+          privacy, please use the contact details below.
+        </p>
+        <h2 id="use-by-children">Use by children</h2>
+        <p>
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity. Javis
+          does not knowingly collect any Personal Identifiable Information from
+          children under the age of 13. If you think that your child provided
+          this kind of information on our website, we strongly encourage you to
+          contact us immediately and we will do our best efforts to promptly
+          remove such information from our records.
+        </p>
+        <h2 id="how-to-contact-us">How to contact us</h2>
+        <p>
+          If you have any questions, concerns or complaints regarding our
+          compliance with this notice and the data protection laws, or if you
+          wish to exercise your rights, or if you do not want us to retain or
+          use your personal information in any way, we encourage you to contact
+          us.
+        </p>
+        <p>
+          The data controller responsible for your information is Initial
+          condition s.r.o., which you can contact online, or by mail at:
+          khacvy93@gmail.com
+        </p>
+      </div>
+    </div>
+  </React.StrictMode>
+);
